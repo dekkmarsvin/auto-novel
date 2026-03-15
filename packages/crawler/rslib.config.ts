eb2888ca7783f06a5f18a205abbb2967eda57750
FishHawk <whang23@outlook.com>
2026-03-15 22:14:16 +0800

    chore(crawler): 将爬虫提出来作为独立库

---
+++ packages/crawler/rslib.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "@rslib/core";
+
+const isDebug = process.env.NODE_ENV !== "production";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "./src",
+    },
+  },
+  lib: [
+    {
+      format: "esm",
+      dts: true,
+      bundle: true,
+      source: {
+        entry: { index: "src/index.ts" },
+        tsconfigPath: "tsconfig.json",
+      },
+      output: {
+        target: "web",
+      },
+    },
+  ],
+  output: {
+    cleanDistPath: true,
+    sourceMap: isDebug,
+    minify: !isDebug,
+  },
+});
