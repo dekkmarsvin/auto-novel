0f4a7d0b50f90ed72aac3ea07b05cbe2a37bf85e
FishHawk <whang23@outlook.com>
2026-04-20 09:50:16 +0800

    chore(web): 升级到vite8

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,11 @@
 import vue from '@vitejs/plugin-vue';
 import Sonda from 'sonda/vite';
 import AutoImport from 'unplugin-auto-import/vite';
-import imagemin from 'unplugin-imagemin/vite';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import type { UserConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -78,13 +76,14 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    resolve: {
+      tsconfigPaths: true,
+    },
     plugins: [
       vue(),
-      imagemin({}),
       createHtmlPlugin({
         minify: { minifyJS: true },
       }),
-      tsconfigPaths({ loose: true }),
       AutoImport({
         dts: 'src/auto-imports.d.ts',
         ignore: ['h'],
